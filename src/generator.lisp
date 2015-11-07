b044435b73b9bdea014d20ec9da45b6ef70f9587
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-07 18:07:04 -0300

    Change package names, drop Dylan naming convention

--- src/generator.lisp
+++ src/generator.lisp
@@ -1,78 +1,83 @@
 (in-package :cl-user)
-(defpackage project-gen.gen
+(defpackage project-generator
   (:use :cl :trivial-types :anaphora)
-  (:export :<file>
-           :path
-           :content
-           :<option>
-           :name
-           :docstring
-           :requiredp
-           :default
-           :<template>
-           :name
-           :options
-           :files
-           :<missing-required-option>
-           :validate-options
-           :render))
-(in-package :project-gen.gen)
+  ;; Classes and accessors
+  (:export :file
+           :file-path
+           :file-content
+           :option
+           :option-name
+           :option-docstring
+           :option-required-p
+           :option-default
+           :template
+           :template-name
+           :template-options
+           :template-files
+           :missing-required-option)
+  ;; Interface
+  (:export :validate-options
+           :render
+           :list-templates
+           :register-template))
+(in-package :project-generator)
 
-(defclass <file> ()
-  ((path :reader path
+(defclass file ()
+  ((path :reader file-path
          :initarg :path
          :type string
          :documentation "The path to the file, a Mustache template string.")
-   (content :reader content
+   (content :reader file-content
             :initarg :content
             :type string
             :documentation "The file's contents, a Mustache template string."))
   (:documentation "A file."))
 
-(defclass <option> ()
-  ((name :reader name
+(defclass option ()
+  ((name :reader option-name
          :initarg :name
          :type keyword
          :documentation "The name of the option.")
-   (docstring :reader docstring
+   (docstring :reader option-docstring
               :initarg :docstring
               :type string
               :documentation "The option's documentation string.")
-   (requiredp :reader requiredp
+   (requiredp :reader option-required-p
               :initarg :requiredp
               :initform nil
               :type boolean
               :documentation "Whether the option is required.")
-   (processor :reader processor
+   (processor :reader option-processor
               :initarg :processor
               :initform (lambda (x) x)
               :type function
               :documentation "A function used to process the value given to the option.")
-   (default :reader default
-            :initarg :default
+   (default :reader option-default
+            :initarg
+            :default
             :documentation "The option's default value."))
   (:documentation "An option to a template."))
 
-(defclass <template> ()
-  ((name :reader name
+(defclass template ()
+  ((name :reader template-name
          :initarg :name
          :type string
          :documentation "The template's descriptive name.")
-   (docstring :reader docstring
+   (docstring :reader template-docstring
               :initarg :docstring
               :type string
               :documentation "The template's documentation string.")
-   (options :reader options
+   (options :reader template-options
             :initarg :options
-            :type (proper-list <option>)
+            :type (proper-list option)
             :documentation "A list of template options.")
-   (files :reader files
+   (files :reader template-files
           :initarg :files
           :type (proper-list files)
           :documentation "A list of files to template."))
   (:documentation "Represents a template."))
 
-(define-condition <missing-required-option> (simple-error)
+(define-condition missing-required-option (simple-error)
   ((option-name :reader option-name
                 :initarg :option-name
                 :type keyword
@@ -82,24 +87,24 @@
      (format stream "The option '~A' was required but was not supplied."
              (option-name condition)))))
 
-(defmethod validate-options ((template <template>) (options list))
+(defmethod validate-options ((template template) (options list))
   "Take a plist of options, and validate it against the options in the template."
   (let ((final-options (list)))
-    (loop for option in (options template) do
-      (aif (getf options (name option))
+    (loop for option in (template-options template) do
+      (aif (getf options (option-name option))
            ;; Was the option supplied? If so, apply the option's processor to it
            ;; and add it to the `final-options` list
-           (setf (getf final-options (name option))
-                 (funcall (processor option) it))
+           (setf (getf final-options (option-name option))
+                 (funcall (option-processor option) it))
            ;; Otherwise, check if it has a default value
            (if (slot-boundp option 'default)
                ;; Use this value
-               (setf (getf final-options (name option))
-                     (default option))
+               (setf (getf final-options (option-name option))
+                     (option-default option))
                ;; No default? If the option is required, signal an error
-               (if (requiredp option)
-                   (error '<missing-required-option>
-                          :option-name (name option))))))
+               (if (option-required-p option)
+                   (error 'missing-required-option
+                          :option-name (option-name option))))))
     final-options))
 
 (defun render-template (template-string data)
@@ -110,21 +115,33 @@
        (cons (first sublist) (second sublist)))
      str)))
 
-(defmethod render ((template <template>) (options list) (directory pathname))
+(defmethod render ((template template) (options list) (directory pathname))
   (let ((options (validate-options template options))
-        (directory (fad:pathname-as-directory directory)))
+        (directory (uiop:ensure-directory-pathname directory)))
     (setf (getf options :year)
           (write-to-string (local-time:timestamp-year (local-time:now))))
-    (loop for file in (files template) do
-      (let* ((file-path (parse-namestring (render-template (path file) options)))
-             (full-file-path (merge-pathnames
-                              file-path
-                              directory))
-             (content (render-template (content file) options)))
+    (loop for file in (template-files template) do
+      (let* ((file-path (parse-namestring (render-template (file-path file) options)))
+             (full-file-path (merge-pathnames file-path
+                                              directory))
+             (content (render-template (file-content file) options)))
         (ensure-directories-exist
-         (fad:pathname-directory-pathname full-file-path))
+         (uiop:pathname-directory-pathname full-file-path))
         (with-open-file (output-stream full-file-path
                                        :direction :output
                                        :if-exists :supersede
                                        :if-does-not-exist :create)
           (write-string content output-stream))))))
+
+(defvar *templates* (list)
+  "The list of templates.")
+
+(defun list-templates ()
+  "Return a list of project templates."
+  *templates*)
+
+(defun register-template (template)
+  "Register a template."
+  (declare (type template template))
+  (push template *templates*)
+  template)
--- src/skeletons/generic/generic.lisp
+++ src/skeletons/generic/generic.lisp
@@ -1,60 +1,68 @@
 (in-package :cl-user)
-(defpackage project-gen.skel.generic
-  (:use :cl :project-gen.gen)
-  (:import-from :project-gen.util
-                :read-skeleton)
+(defpackage project-generator.skeleton.generic
+  (:use :cl)
+  (:import-from :project-generator
+                :template
+                :option
+                :file)
+  (:import-from :project-generator.util
+                :read-skeleton
+                :parse-systems-list)
   (:export :+generic-template+))
-(in-package :project-gen.skel.generic)
+(in-package :project-generator.skeleton.generic)
 
 (defparameter +generic-template+
   (make-instance
-   '<template>
+   'template
    :name "Generic Project"
    :docstring "An empty, generic ASDF system."
    :options
-   (list (make-instance '<option>
+   (list (make-instance 'option
                         :name :name
                         :requiredp t
                         :docstring "The project's name.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :author
                         :requiredp t
                         :docstring "The project author's name.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :email
                         :docstring "The project author's email.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :homepage
                         :docstring "The project's homepage.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :license
                         :requiredp t
                         :docstring "The project's license string, e.g. 'MIT', 'GPLv3'.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :description
                         :docstring "A short, one-line description of the project.")
-         (make-instance '<option>
+         (make-instance 'option
                         :name :dependencies
                         :processor (lambda (deps)
                                      (format nil "~{:~A~^~%               ~}"
-                                             (project-gen.util:parse-systems-list deps)))
+                                             (parse-systems-list deps)))
                         :docstring "The project's dependent systems, as a comma-separated list, e.g: 'local-time, lucerne, crane'."))
    :files
-   (list (make-instance '<file>
+   (list (make-instance 'file
                         :path "{{name}}.asd"
                         :content (read-skeleton #p"generic/asdf.lisp"))
-         (make-instance '<file>
+         (make-instance 'file
                         :path "{{name}}-test.asd"
                         :content (read-skeleton #p"generic/asdf-test.lisp"))
-         (make-instance '<file>
+         (make-instance 'file
                         :path "src/{{name}}.lisp"
                         :content (read-skeleton #p"generic/source.lisp"))
-         (make-instance '<file>
+         (make-instance 'file
                         :path "t/{{name}}.lisp"
                         :content (read-skeleton #p"generic/test.lisp"))
-         (make-instance '<file>
+         (make-instance 'file
                         :path "README.md"
                         :content (read-skeleton #p"generic/README.md"))
-         (make-instance '<file>
+         (make-instance 'file
                         :path ".gitignore"
-                        :content (read-skeleton #p"generic/gitignore.txt")))))
+                        :content (read-skeleton #p"generic/gitignore.txt"))
+         (make-instance 'file
+                        :path ".travis.yml"
+                        :content (read-skeleton #p"generic/travis.yml")))))
